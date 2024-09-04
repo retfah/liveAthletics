0b493760dcdb1e2e3dd6176c0397e7b409534523
Reto Fahrni <82324518+retfah@users.noreply.github.com>
2024-09-04 22:55:18 +0200

    Cleanup 3

--- testExport.js
+++
@@ -1,6 +0,0 @@
-
-const vals = [1,2,4,8];
-export function getVals(){
-    // does this return the vals array of this module when not imported?; or would have to import it separately?
-    return vals;
-}
